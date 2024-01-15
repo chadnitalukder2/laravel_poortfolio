3ad383840668b1ed1a42650f5cfbb2bc52fe5796
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-01-15 23:22:51 +0600

    profile-page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,5 +18,17 @@ class AdminController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/login');
+    } //End Method  
+
+    public function Profile(){
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+
+        //profile-page e dd hobe
+        //dd($adminData->toArray()); 
+        return view('admin.admin_profile_view', compact('adminData'));
+
+   
+
     } //End Method  
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/dashboard', function () {
 //admin all route
 Route::controller(AdminController::class)->group(function(){
     Route::get('/admin/logout', 'destroy')->name('admin.logout');
+    Route::get('/admin/profile', 'Profile')->name('admin.profile');
 });
 
 // Route::middleware('auth')->group(function () {
