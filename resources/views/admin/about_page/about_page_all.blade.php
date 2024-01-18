7e38119a1764635ef66fa3b4f87383de313bb8fc
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-01-18 16:45:36 +0600

    about page

--- resources/views/admin/about_page/about_page_all.blade.php
+++ resources/views/admin/about_page/about_page_all.blade.php
@@ -32,7 +32,7 @@
                                 </div>
                             </div>
                              <!-- end row -->
-                             
+
                              <div class="row mb-3">
                                 <label for="example-text-input" class="col-sm-2 col-form-label">Short Description</label>
                                 <div class="col-sm-10">
@@ -56,6 +56,7 @@
                                 </div>
                             </div>
                              <!-- end row -->
+
                              <div class="row mb-3">
                                 <div class="col-sm-10">
                                     <label for="example-text-input" class="col-sm-2 col-form-label"></label>
@@ -63,6 +64,7 @@
                                 </div>
                             </div>
                              <!-- end row -->
+                             
                              <input type="submit" class="btn btn-info waves-effect waves-light " value="Update Slide">
                         </form>
                     </div>
