c7174c4b00593513faf98496a2aee739400c7127
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-01-23 21:11:34 +0600

    delete Portfolio

---
+++ resources/views/frontend/home_all/portfolio.blade.php
@@ -0,0 +1,99 @@
+
+
+<section class="portfolio">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-6 col-lg-8">
+                <div class="section__title text-center">
+                    <span class="sub-title">04 - Portfolio</span>
+                    <h2 class="title">All creative work</h2>
+                </div>
+            </div>
+        </div>
+     
+    </div>
+    <div class="tab-content" id="portfolioTabContent">
+     
+        <div class="tab-pane" id="graphic" role="tabpanel" aria-labelledby="graphic-tab">
+            <div class="container">
+                <div class="row gx-0 justify-content-center">
+                    <div class="col">
+                        <div class="portfolio__active">
+                            <div class="portfolio__item">
+                                <div class="portfolio__thumb">
+                                    <img src="assets/img/portfolio/portfolio_img02.jpg" alt="">
+                                </div>
+                                <div class="portfolio__overlay__content">
+                                    <span>Web Design</span>
+                                    <h4 class="title"><a href="portfolio-details.html">Graphic Design Management System</a></h4>
+                                    <a href="portfolio-details.html" class="link">Case Study</a>
+                                </div>
+                            </div>
+                            <div class="portfolio__item">
+                                <div class="portfolio__thumb">
+                                    <img src="assets/img/portfolio/portfolio_img01.jpg" alt="">
+                                </div>
+                                <div class="portfolio__overlay__content">
+                                    <span>Apps Design</span>
+                                    <h4 class="title"><a href="portfolio-details.html">Banking Management System</a></h4>
+                                    <a href="portfolio-details.html" class="link">Case Study</a>
+                                </div>
+                            </div>
+                            <div class="portfolio__item">
+                                <div class="portfolio__thumb">
+                                    <img src="assets/img/portfolio/portfolio_img03.jpg" alt="">
+                                </div>
+                                <div class="portfolio__overlay__content">
+                                    <span>UX/UI Design</span>
+                                    <h4 class="title"><a href="portfolio-details.html">Banking Management System</a></h4>
+                                    <a href="portfolio-details.html" class="link">Case Study</a>
+                                </div>
+                            </div>
+                            <div class="portfolio__item">
+                                <div class="portfolio__thumb">
+                                    <img src="assets/img/portfolio/portfolio_img04.jpg" alt="">
+                                </div>
+                                <div class="portfolio__overlay__content">
+                                    <span>Web Development</span>
+                                    <h4 class="title"><a href="portfolio-details.html">Banking Management System</a></h4>
+                                    <a href="portfolio-details.html" class="link">Case Study</a>
+                                </div>
+                            </div>
+                            <div class="portfolio__item">
+                                <div class="portfolio__thumb">
+                                    <img src="assets/img/portfolio/portfolio_img05.jpg" alt="">
+                                </div>
+                                <div class="portfolio__overlay__content">
+                                    <span>Web Development</span>
+                                    <h4 class="title"><a href="portfolio-details.html">Banking Management System</a></h4>
+                                    <a href="portfolio-details.html" class="link">Case Study</a>
+                                </div>
+                            </div>
+                            <div class="portfolio__item">
+                                <div class="portfolio__thumb">
+                                    <img src="assets/img/portfolio/portfolio_img06.jpg" alt="">
+                                </div>
+                                <div class="portfolio__overlay__content">
+                                    <span>Web Development</span>
+                                    <h4 class="title"><a href="portfolio-details.html">Banking Management System</a></h4>
+                                    <a href="portfolio-details.html" class="link">Case Study</a>
+                                </div>
+                            </div>
+                            <div class="portfolio__item">
+                                <div class="portfolio__thumb">
+                                    <img src="assets/img/portfolio/portfolio_img07.jpg" alt="">
+                                </div>
+                                <div class="portfolio__overlay__content">
+                                    <span>Web Development</span>
+                                    <h4 class="title"><a href="portfolio-details.html">Banking Management System</a></h4>
+                                    <a href="portfolio-details.html" class="link">Case Study</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</section>
