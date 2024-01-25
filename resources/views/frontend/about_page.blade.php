5ee30d4502c394019d207cfc4e6c0cd1585b4404
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-01-25 14:50:11 +0600

    title

--- resources/views/frontend/about_page.blade.php
+++ resources/views/frontend/about_page.blade.php
@@ -1,6 +1,8 @@
 @extends('frontend.main_master')
 @section('main')
-
+@section('title')
+    About | Website
+@endsection
   
 <main>
 
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,5 +1,9 @@
 @extends('frontend.main_master')
 @section('main')
+
+@section('title')
+    Home | Website
+@endsection
     
 <!-- banner-area -->
 @include('frontend.home_all.home_slide')
